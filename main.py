9f21d16c40f7abd9bf3f72fe99708a05d206345c
ahmedalnouby132 <qwertyse122@gmail.com>
2020-03-04 16:04:24 +0200

    Modifying Getting Function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import UI
 import wave
-import untitled
+#import untitled
 from PyQt5 import QtWidgets, QtCore
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
@@ -17,18 +17,18 @@ class ApplicationWindow(UI.Ui_MainWindow):
     def __init__(self,mainWindow):
         super(ApplicationWindow,self).setupUi(mainWindow)
         
-        self.data=None
+        self.oneChannelData=None
         self.FileName=None
         self.Path=None
         self.FFT=None
         self.freqs=None 
-        
+        self.oneChannelData=None
+
         self.OpenFileButton.clicked.connect(self.getData)
         self.OpenFileButton.clicked.connect(self.graph)
         self.PlayButton.clicked.connect(self.generateSound)
         self.PauseButton.clicked.connect(self.pauseSound)
         self.SaveButton.clicked.connect(self.saveSoundFile)
-
         
     def getData(self):
         #get the file and read it#
@@ -36,57 +36,38 @@ class ApplicationWindow(UI.Ui_MainWindow):
         dataFromTheFile=getDataFromAFile(self.filePath)
         Output,self.FileName,self.Path = dataFromTheFile if dataFromTheFile is not None  else  [None,self.FileName]
         if Output is not None:
-            print(self.data)
-            self.data,self.SampleRate=Output
-            print(len(self.data)*0.00095437989)
-            realData=self.data[:,0]
-            imagData=self.data[:,1]
-            self.complexData=realData + 1j * imagData #np.vectorize(complex)(realData*1000, imagData)
+            nChannelData,self.SampleRate=Output
+            print(isinstance( nChannelData[0] ,np.ndarray))
+            if  isinstance( nChannelData[0] ,np.ndarray):
+                self.oneChannelData=nChannelData[:,0]
+            else:
+                self.oneChannelData=nChannelData
             self.graph()
         
     def graph(self):
-        if self.data is not None:
-            self.FFT=fftpk.fft(self.complexData)
+        if self.oneChannelData is not None:
+            self.FFT=fftpk.fft(self.oneChannelData)
             self.freqs =fftpk.fftfreq(len(self.FFT),(1.0/self.SampleRate))
             self.widget.plotItem.plot(self.freqs[range(len(self.FFT)//2)],abs(self.FFT[range(len(self.FFT)//2)]))
-            self.IFFT=fftpk.ifft(self.FFT)
-
     
     
     def generateSound(self):
-        if self.data is not None:
+        if self.oneChannelData is not None:
             generatedAudio=np.real(fftpk.ifft(self.FFT))
             sd.play(generatedAudio,self.SampleRate)
             
     def pauseSound(self):
-        if (self.data is not None):
+        if (self.oneChannelData is not None):
             sd.stop()
     
     def saveSoundFile(self):
-        if self.data is not None:
-            GenratedAudio=fftpk.ifft(self.FFT)
-            GenratedAudio=np.real(GenratedAudio)
-            """SavedData=([],self.SampleRate)
-            for frame in GenratedAudio:
-                SavedData[0].extend([[np.real(frame),np.imag(frame)]])
-            print(SavedData)
-            SavedData=np.array(SavedData[0][:,0])"""
+        
+        if self.oneChannelData is not None:
+            GenratedAudio=np.real(fftpk.ifft(self.FFT))
             SavedData=GenratedAudio
             name= QtGui.QFileDialog.getSaveFileName( None,'Save File',self.Path+".wav")[0]
             print(name,self.SampleRate,SavedData)
-            sio.wavfile.write(name, self.SampleRate//2, SavedData.astype(np.dtype('i2')))
-            """wf = wave.open(name, 'wb')
-            #wf.setnchannels(channels)
-            duration=3
-            wf.setsampwidth(3)
-            wf.setframerate(self.SampleRate)
-            GenratedAudio=fftpk.ifft(self.FFT)
-            SavedData=[]
-            for frame in GenratedAudio:
-                SavedData.extend([[np.real(frame),np.imag(frame)]])
-            wf.setnframes(int(self.SampleRate * duration))
-            wf.writeframes(SavedData)
-            wf.close()"""
+            sio.wavfile.write(name, self.SampleRate, SavedData)
         #getOpenFileName(None,  'load', "./","All Files *;;" "*.wav;;" " *.mp3;;" "*.snd")
             
             
