39145ddce5d9865f7a891312485141f80de53031
ahmedalnouby132 <qwertyse122@gmail.com>
2020-03-05 10:56:11 +0200

    update

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ import scipy.fftpack as fftpk
 import sounddevice as sd
 import cmath
 import Colors
-
+from PopUpWindowClass import BuildPopUpWindow
+#import 
 
 #WinFn(Win_Fn,freq,Bands,Gains,data)
 
 #Init a gain for each band
-Gains = np.ones([10])
+Gains = [0,0,0,0,0,0,0,0,0,0]
 
 #Freq. Spectrum Division (Setting Bands)
 Bands = np.array([[20,40],[40,80],[80,160]
@@ -28,43 +29,46 @@ Bands = np.array([[20,40],[40,80],[80,160]
 #Supported Window Functions
 Win_Fn = np.array(['Rectangular','Hamming','Hanning'])
 
+#Saving Modes#
+SaveMode=np.array(["To A File","Save1","Save2"])
+
 
 class ApplicationWindow(UI.Ui_MainWindow):
     def __init__(self,mainWindow):
         super(ApplicationWindow,self).setupUi(mainWindow)
-        
+        self.VariableInitialization()
+        self.ButtonInitialization()
+        self.sliderInitialization()
+
+    def VariableInitialization(self):
+        self.slidersEnable=False
+        self.PopUpMenuIsON=False
         self.FileName=None
         self.Path=None
         self.freqs=None 
         self.FFTdata=None
         self.SampleRate=None
-
+        self.MainData=[self.FFTdata,self.freqs,self.SampleRate,self.FileName,self.Path]
+        self.SavedData1=[]
+        self.SavedData2=[]
+            
+    def ButtonInitialization(self):
         self.OpenFileButton.clicked.connect(self.OpenFile)
         self.OpenFileButton.clicked.connect(self.graphMainData)
         self.PlayButton.clicked.connect(self.generateSound)
         self.PauseButton.clicked.connect(self.pauseSound)
         self.SaveButton.clicked.connect(self.saveSoundFile)
-        self.CompareButton.clicked.connect(self.compareToAFile)
-        self.ClearComparedButton.clicked.connect(self.clearCompared)
-
-        self.sliders=[
-            self.Band1Slider,
-            self.Band2Slider,
-            self.Band3Slider,
-            self.Band4Slider,
-            self.Band5Slider,
-            self.Band6Slider,
-            self.Band7Slider,
-            self.Band8Slider,
-            self.Band9Slider,
-            self.Band10Slider
-        ]
-        
+        self.CompareButton.clicked.connect(self.OpenPopUpWindow)
+        #self.CompareButton.clicked.connect(self.compareToAFile)
+        #self.ClearComparedButton.clicked.connect(self.clearCompared)
+        self.OnOff.clicked.connect(self.slidersChangeState)
 
     def OpenFile(self):
         Output = self.getData()
         if Output is not None:
             [self.FFTdata,self.freqs,self.SampleRate,self.FileName,self.Path]= Output
+            print(self.MainData)
+            print(self.FFTdata,self.freqs,self.SampleRate,self.FileName,self.Path)
 
     def getData(self):
         #to stop playing the old music#
@@ -107,11 +111,24 @@ class ApplicationWindow(UI.Ui_MainWindow):
     
     def saveSoundFile(self):
         if self.FFTdata is not None:
-            GenratedAudio=np.real(fftpk.ifft(self.FFTdata))
-            SavedData=GenratedAudio
-            name= QtGui.QFileDialog.getSaveFileName( None,'Save File',self.Path+".wav")[0]
-            print(name,self.SampleRate,SavedData)
-            sio.wavfile.write(name, self.SampleRate, SavedData)
+            indexOfSaveModes=self.SaveMode.currentIndex()
+            if SaveMode[indexOfSaveModes]=="To A File":
+                GenratedAudio=np.real(fftpk.ifft(self.FFTdata))
+                SavedData=GenratedAudio
+                name= QtGui.QFileDialog.getSaveFileName( None,'Save File',self.Path+".wav")[0]
+                print(name,self.SampleRate,SavedData)
+                sio.wavfile.write(name, self.SampleRate, SavedData)
+            if SaveMode[indexOfSaveModes]=="Save1":
+                self.SavedData1=self.MainData
+            if SaveMode[indexOfSaveModes]=="Save2":
+                self.SavedData2=self.MainData
+
+    def OpenPopUpWindow(self):
+        BuildPopUpWindow(self.MainData,self.SavedData1,self.SavedData2)
+        
+
+
+
 
     def compareToAFile(self):
         if self.FFTdata is not None:
@@ -124,18 +141,71 @@ class ApplicationWindow(UI.Ui_MainWindow):
             self.widget.plotItem.clear()
             self.graphMainData()
 
-    """def ChangeSlider(self):
-        if self.data is not None:"""
-            
-            
 
-            
-            
+
+    def sliderInitialization(self):
+
+        self.sliders=[
+            self.Band1Slider,
+            self.Band2Slider,
+            self.Band3Slider,
+            self.Band4Slider,
+            self.Band5Slider,
+            self.Band6Slider,
+            self.Band7Slider,
+            self.Band8Slider,
+            self.Band9Slider,
+            self.Band10Slider
+        ]
+
+        self.slidersEnable=False
+        self.OnOff.setText("ON")
+
+        self.sliders[0].valueChanged.connect(lambda :self.edittingSliderValue(0))
+        self.sliders[1].valueChanged.connect(lambda :self.edittingSliderValue(1))
+        self.sliders[2].valueChanged.connect(lambda :self.edittingSliderValue(2))
+        self.sliders[3].valueChanged.connect(lambda :self.edittingSliderValue(3))
+        self.sliders[4].valueChanged.connect(lambda :self.edittingSliderValue(4))
+        self.sliders[5].valueChanged.connect(lambda :self.edittingSliderValue(5))
+        self.sliders[6].valueChanged.connect(lambda :self.edittingSliderValue(6))
+        self.sliders[7].valueChanged.connect(lambda :self.edittingSliderValue(7))
+        self.sliders[8].valueChanged.connect(lambda :self.edittingSliderValue(8))
+        self.sliders[9].valueChanged.connect(lambda :self.edittingSliderValue(9))
+
+        
+        for numberOfBand in range(0,10):
+            self.sliders[numberOfBand].setRange(0,100)
+            self.sliders[numberOfBand].setValue(50)
+            self.sliders[numberOfBand].setDisabled(True)
+            #self.sliders[numberOfBand].valueChanged.connect(lambda :self.edittingSliderValue(numberOfBand))
+
+    def slidersChangeState(self):
+        if self.FFTdata is not None:
+            if self.slidersEnable==False:
+                for numberOfBand in range(0,10):
+                    self.sliders[numberOfBand].setEnabled(True)
+                windowMode=Win_Fn[self.WindowMode.currentIndex()]
+                print(windowMode)
+                self.WindowMode.setDisabled(True)
+                self.slidersEnable=True
+                self.OnOff.setText("OFF")
+            else:
+                for numberOfBand in range(0,10):
+                    self.sliders[numberOfBand].setDisabled(True)
+                    self.sliders[numberOfBand].setValue(50)
+                self.WindowMode.setEnabled(True)
+                self.slidersEnable=False
+                self.OnOff.setText("ON")
+
+    def edittingSliderValue(self,numberOfBand): 
+        Gains[numberOfBand] = self.sliders[numberOfBand].value()/50
+
+
         
 def main():
     app=QtWidgets.QApplication(sys.argv)
     mainWindow=QtWidgets.QMainWindow()
-    ui=ApplicationWindow(mainWindow)
+    ApplicationWindow(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
     
