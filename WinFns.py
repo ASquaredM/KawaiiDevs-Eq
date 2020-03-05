ccc29986a9e16cc31faa829ca3999c52071b8ab8
ahmedalnouby132 <qwertyse122@gmail.com>
2020-03-05 14:56:52 +0200

    Final

--- WinFns.py
+++ WinFns.py
@@ -11,27 +11,30 @@ Win_Fn = np.array(['Rectangular','Hamming','Hanning'])
 """
 
 def WinFn(Win_Fn,freq,Bands,Gains,data):
-    """print(freq)
+    print(freq)
     print(data)
-    lendata=len(data)"""
-    positiveData=data#[lendata//2:]
-    halfOfRange=freq#[lendata//2:]
+    lendata=len(data)
+    positiveData=data[:lendata//2]
+    halfOfRange=freq[:lendata//2]
     if Win_Fn == 'Rectangular':
         Win_data=Rec_Fn(positiveData,halfOfRange,Bands,Gains)
     elif Win_Fn == 'Hamming':
         Win_data=Ham_Fn(positiveData,halfOfRange,Bands,Gains)
     elif Win_Fn == 'Hanning':
         Win_data=Han_Fn(positiveData,halfOfRange,Bands,Gains)
-    """print(Win_data)
+    print(Win_data)
     positiveGainedData=np.array(Win_data,dtype=complex)#[len(Win_data)]
     print(positiveData)
     zeros=np.zeros(lendata//2,dtype=complex) 
-    print(zeros) """
+    print(zeros) 
     #positiveData2 = [zeros,positiveData]  
-    """positiveDataFinal=np.concatenate([zeros,positiveGainedData])
+    positiveDataFinal=np.concatenate([positiveGainedData,zeros])
     FlippedDate=np.flip(positiveGainedData)
-    negativeDataFinal=np.concatenate([FlippedDate,zeros])
-    Win_data=positiveDataFinal+negativeDataFinal"""
+    negativeDataFinal=np.concatenate([zeros,FlippedDate])
+    Win_data=positiveDataFinal+negativeDataFinal
+    print(len(Win_data))
+    print(len(freq))
+    freq = np.array( freq [0:len(freq)-1])
     return [Win_data,freq]
 
 def Rec_Fn(data,freq,Bands,Gains):
